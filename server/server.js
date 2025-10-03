1ba198dcc8c0ecd50f97b485c4212eaedfbe12f8
suryanshu0-coder <110730590+suryanshu0-coder@users.noreply.github.com>
2025-10-04 00:49:48 +0545

    added frontend url

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 const app = express();
 app.use(cors());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://quickkcv.vercel.app" }));
 app.use(express.json());
 app.use(express.static("public")); // Serve static files from public directory
 
